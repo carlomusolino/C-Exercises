66d5a8982b27cf726729b6e2ab701971cdeab7eb
Carlo Musolino <carlomusolino@Carlos-MacBook-Pro.local>
2021-11-29 17:52:43 +0100

    Fixed a bug where clang would not recognise std::pow

--- sheet4/root_finding/root_finding_client.cpp
+++ sheet4/root_finding/root_finding_client.cpp
@@ -24,7 +24,7 @@ int main() {
     * auto means that the compiler needs to be able to deduce the type from the initialisation --> You ALWAYS need
     * to initialise a variable declared as auto.
     */
-    auto func = [](double x) -> double {return (std::pow(x,3) - 2 * x + 5);} ;
+    auto func = [](double x) -> double {return (pow(x,3) - 2 * x + 5);} ;
     cout << "f(x) = x^3 - 2x + 5" << endl;
     /* std::tie creates a tuple of references : https://en.cppreference.com/w/cpp/utility/tuple/tie
     * This is why we use std::make_tuple in the function return statement! 
@@ -42,7 +42,7 @@ int main() {
     cout << "Root up to a bunch of significant digits (according to Mathematica): " << -2.09455148154233 << endl ;
 
     x_low = 1.7; x_hi=5.;
-    auto func2 = [](double x) -> double {return (std::pow(x,5) - 7 * std::pow(x,3) + 9);} ;
+    auto func2 = [](double x) -> double {return (pow(x,5) - 7 * pow(x,3) + 9);} ;
 
     std::tie(root,niter) = root_finding<double>(func2,x_low,x_hi);
     cout << "Non recursive:" << endl;
